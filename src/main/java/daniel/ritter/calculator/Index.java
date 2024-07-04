bbcce3452f8fd5efd3048f7b8129006f0fd581c2
danie <danie@Daniel>
2024-07-04 09:36:10 +0200

    Projekterstellung

---
+++ src/main/java/daniel/ritter/calculator/Index.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daniel.ritter.calculator;
+
+/**
+ *
+ * @author danie
+ */
+public class Index{
+
+    public static void main(String[] args) {
+        Calculator gui = new Calculator();
+        gui.setVisible(true);
+    }
+}
